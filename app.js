01d69dcd9f80aae1bba219a2580e20eb534064e3
Emilis01 <emilisdov@yahoo.com>
2025-01-22 11:48:57 +0200

    testas 8

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8080;
 app.use(cors());
 // KOMENTARASss
 // asdasdasdasdasd
-app.get('/', (req, res) => res.send(`Hello World from PagaliauSSS kazkas pavyko 5 ! Dabartinis laikas yra ${new Date().toLocaleDateString()}`));
+app.get('/', (req, res) => res.send(`Hello World from PagaliauSSS kazkas pavyko 8 ! Dabartinė data yra ${new Date().toLocaleDateString()}`));
 app.get('/user', (req, res) => res.send({
 	name: "petras",
 	email: "petras@gmail.com",
