1afa22f7cdf17189ab2ed7d116277589c7e7f25c
Emilis01 <your.emilisdov@yahoo.com>
2025-04-26 15:48:47 +0300

    Pataisyta: teisingas add funkcijos importas į testą

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,5 @@
-try {
-  const { add } = require('../app');
+const { add } = require('../app');
 
-  test('add function should return the sum of two numbers', () => {
+test('add function should return the sum of two numbers', () => {
     expect(add(2, 3)).toBe(5);
-  });
-} catch (e) {
-  test.skip('add function test skipped because function is missing', () => {});
-}
+});
