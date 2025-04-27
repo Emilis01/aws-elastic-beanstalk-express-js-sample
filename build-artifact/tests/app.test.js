3f736d6e7fe1eeb27802fcdc21ce3522306d2c00
Emilis01 <your.emilisdov@yahoo.com>
2025-04-27 16:09:42 +0300

    Pašalinti build-artifact ir .zip failai

--- build-artifact/tests/app.test.js
+++
@@ -1,5 +0,0 @@
-const { add } = require('../app');
-
-test('add function should return the sum of two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
--- build-artifact/tests/basic.test.js
+++
@@ -1,4 +0,0 @@
-test('basic math should work', () => {
-  expect(2 + 2).toBe(4);
-});
-
